fb9620aff355e1ebf3d4501f74f7cbd6f97b69f2
Richard Schneider <makaretu@gmail.com>
2016-11-01 17:46:03 +1300

    feat(money): construct money with an amount and a ISO-4217 currency code

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # money-works
 Work with money in multiple currencies
+
+Not yet ready for prime time
--- test/money.js
+++ test/money.js
@@ -1,9 +1,49 @@
 'use strict';
 
-//const Money = require('..');
-//const Big = require('big.js');
-//const should = require('should');
+const Money = require('..');
+const Big = require('big.js');
+const should = require('should');
 
 describe('Money', () => {
+
+    it('should have a Big amount', () => {
+        let money = new Money(10, 'YEN');
+        money.should.have.property('amount');
+        money.amount.should.be.instanceof(Big);
+        money.amount.should.eql(new Big(10));
+    });
+
+    it('should have an ISO-4217 currency code', () => {
+        let money = new Money(10, 'YEN');
+        money.should.have.property('currency', 'YEN');
+    });
+
+    describe('constructor', () => {
+
+        it('should allow an integer amount', () => {
+            new Money(10, 'NZD').should.have.property('amount', new Big(10));
+        });
+
+        it('should allow a floating point amount', () => {
+            new Money(10.123456789, 'NZD').should.have.property('amount', new Big(10.123456789));
+        });
+
+        it('should allow a Big amount', () => {
+            new Money(new Big(10.123456789), 'NZD').should.have.property('amount', new Big(10.123456789));
+        });
+
+        it('should not allow a string amount', () => {
+            should.throws(() => new Money('10.12', 'NZD'));
+        });
+
+        it('should allow ISO-4217 currency code', () => {
+            let ok = new Money(0, 'NZD');
+            ok.should.be.an.instanceof(Money);
+
+            should.throws(() => new Money(0, 'nzd'));
+            should.throws(() => new Money(0, 'NZDX'));
+        });
+
+    });
 
 });
